3a3228d3a70684bc3a25c5464d5c14045bd02876
Anika126 <110670880+Anika126@users.noreply.github.com>
2022-12-23 22:06:31 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -64,9 +64,9 @@ function setup() {
   breakButton.class("breakbutton");
 
   //breakButton.mouseClicked(handleButtonPress);
-  breakButton.mousePressed(handleButtonPress);
+  //breakButton.mousePressed(handleButtonPress);
   //breakButton.mouse(handleButtonPress);
-  //breakButton.mousePressed(ButtonPress);
+  breakButton.mousePressed(ButtonPress);
 
 
 }
